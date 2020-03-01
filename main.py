23bd2595040263b0461d35bee5cd5140f3d1153b
Danielle Lamb <49995394+dkl2036@users.noreply.github.com>
2020-03-01 00:08:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import time
 import sys
 
-div_30 = 0
-div_5 = 0
-div_3 = 0
-div_2 = 0
-div_2_and_3 = 0
-div_none = 0
-
 try_again = True
 
 while try_again:
+    
+    div_30 = 0
+    div_5 = 0
+    div_3 = 0
+    div_2 = 0
+    div_2_and_3 = 0
+    div_none = 0
 
     lower = int(input("Enter lowest number: "))
     higher = int(input("Enter highest number: "))
@@ -18,28 +18,39 @@ while try_again:
     for x in range(lower, higher+1):
         if x % 30 == div_30:
             print(x , "Foo")
+            div_30 += 1
         elif x % 5 == div_5:
             print(x , "Bar")
+            div_5 += 1
         elif x % 3 == div_2_and_3 and x % 2 == div_2_and_3:
             print(x , "FizzBuzz")
+            div_2_and_3 += 1
         elif x % 3 == div_3:
             print(x , "Fizz")
+            div_3 += 1
         elif x % 2 == div_2:
             print(x , "Buzz")
+            div_2 += 1
         else:
             print("Bazz")
+            div_none += 1
 
     total = higher - lower
 
-    print("The total numbers evaluated was " + total + " .")
-    print("There were " , div_30 , " numbers that are divisible by 30.")
-
-try_again = str(input("Would you like to try again? (y/n) "))
-
-if try_again == "y":
-    continue
-else:
-    print("The window will now close.")
-    time.sleep(3)
-    sys.exit()
-    break
+    print("Total numbers evaluated: " + str(total))
+    print("Divisible by 30:",div_30)
+    print("Divisible by 5:",div_5)
+    print("Divisible by 2 and 3:",div_2_and_3)
+    print("Divisible by 3:",div_3)
+    print("Divisible by 2:",div_2)
+    print("Divisible by nothing:",div_none)
+    
+    try_again = str(input("Would you like to try again? (y/n) "))
+    
+    if try_again == "y":
+        continue
+    else:
+        print("The window will now close.")
+        time.sleep(3)
+        sys.exit()
+        break
